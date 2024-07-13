766f3f835c419b315ef8b20bde25cc582a3d6f0e
pereira <gustavosouzapereira4@gmail.com>
2024-07-13 12:35:56 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,12 @@
+value = 0 
+
+age = int(input("What's your age?"))
+
+if age > 17:
+  value = 18.40
+  print(f"Your ticket is U$ {value}")
+  
+else:
+  value = 9.40
+  print(f"Your ticket is U$ {value}")
+  
